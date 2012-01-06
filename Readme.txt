f25a1c5e30135ef7ce6d34ff69e4b34fd065323e
Harsh Bhasin <hersh_b@yahoo.com>
2012-01-06 13:41:44 -0600

    first

---
+++ Readme.txt
@@ -0,0 +1,14 @@
+
+
+Samples to test various JQuery UI widgets (Tab, accordian etc). MVC3 application.
+
+Usage
+-------
+
+1. backup of sample database is in \sql folder. Restore it in Sql Server
+
+2. To change theme: In shared\_layout.cshtml change the tag : @Assets.jgrid("start")  to any of these:dot-luv, humanity, redmond,start,ui-darkness,ui-lightness,blitzer,  dark-hive,eggplant,le-frog, mint-choc,south-street,trontastic
+
+               
+Harsh Bhasin
+http://harshbhasin.com
---
+++ Tab.WebUI/Controllers/SimpleController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Dynamic;
+using System.Web.Script.Serialization;
+using MI.Web.Infrastructure;
+
+namespace MI.WebUI.Controllers
+{
+    public class SimpleController : Controller
+    {
+        //
+        // GET: /Simple///
+
+        public ActionResult Index()
+        {
+            return View();
+           
+        }
+
+        [HttpGet]
+        public ActionResult GetData()
+        {
+            dynamic users = new List<dynamic>();
+
+            users.Add(new ExpandoObject());
+            users[0].Title = "37signals";
+
+            users.Add(new ExpandoObject());
+            users[1].Title = "2012ondazero";
+
+            users.Add(new ExpandoObject());
+            users[2].Title = "AgileForAll";
+
+            users.Add(new ExpandoObject());
+            users[3].Title = "AkitaOnRails";
+
+            users.Add(new ExpandoObject());
+            users[4].Title = "aldonogueira";
+
+            users.Add(new ExpandoObject());
+            users[5].Title = "alexandresoli";
+
+            users.Add(new ExpandoObject());
+            users[6].Title = "AlvaroGarnero";
+
+
+            
+            return VidpubJSON(users);
+        }
+
+        public ActionResult VidpubJSON(dynamic content)
+        {
+            var serializer = new JavaScriptSerializer();
+            serializer.RegisterConverters(new JavaScriptConverter[] { new ExpandoObjectConverter() });
+            var json = serializer.Serialize(content);
+            Response.ContentType = "application/json";
+            return Content(json);
+        }
+    }
+}
